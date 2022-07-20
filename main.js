825d39bab2bab0adf58401c3f9baeb8bd59b373e
Sean Bunch <100799798+SeanBunch@users.noreply.github.com>
2022-07-19 23:19:58 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,31 @@
 const axios = require("../utils/axios");
 const BASE_URL = "https://pokeapi.co/api/v2";
+
+// function getCountry() {
+//   const url = `https://run.mocky.io/v3/4a21cd58-72bb-4360-913e-e9c9fd778c06`;
+//   const result = [];
+//   axios
+//   .get(url)
+//   .then(({data}) => {
+//       const USDcurrencies = data.map((countries) => {
+//           const Name = countries.name.official;
+//           const region = countries.region;
+//         })
+//       const countryCurrencies = USDcurrencies.filter((country) => {
+//           if (country.currencies["USD"] !== undefined) {
+//             console.log(country.currencies["USD"])
+//             // result.push(country.currencies["USD"])
+//               // return country.currencies["USD"];
+//           }
+//       })
+//               return {Name, region, result}
+//       // console.log(USDcurrencies, countryCurrencies);
+//       return USDcurrencies
+//   })
+// }
+
+// console.log(getCountry())
+
 // ====================================================================
 //                  Promise.all() shown below
 // ====================================================================
@@ -14,92 +40,121 @@ const BASE_URL = "https://pokeapi.co/api/v2";
 // ====================================================================
 function showIfExists(pokemonName) {
   const url = `${BASE_URL}/pokemon/${pokemonName}`;
-  let dmg = 0;
   // console.log(url);
   let list = [];
   return (
     axios
       .get(url)
-      // .then((response) => {
-      //   dmg += 1
-      //   console.log(response)
-      //   // console.log(response.data.stats[0].stat.name, response.data.stats[0].base_stat,`dmg-->${dmg}`);
-
-      //   // return response.data.stats[0].stat.name, response.data.stats[0].base_stat,`dmg-->${dmg}`;
-      // })
       .then(({ data }) => {
-        dmg += 1;
-        console.log(data)
-        // console.log(data.stats[0].stat.name, data.stats[0].base_stat,`dmg--> ${dmg}`);
-        // console.log(data.results[0].url)
+        // console.log(data);
         const urlList = data.results.map((results) => {
           list.push(results.url);
-          // list.push(results.url)
-          // return resHold;
         });
-        console.log(list);
-        // console.log(urlList)
-        // return data.stats[0].stat.name, data.stats[0].base_stat,`dmg--> ${dmg}`;
+        // console.log(list);
         return list;
       })
       .then((list) => {
-        const arrPromise= []; 
-
-       for (let i = 0; i < list.length; i++) {
-        const promise = axios
-        .get(list[i])
-        .then(({data}) => {
-          console.log(`${data.name}: Base ${data.stats[0].stat.name} is ${data.stats[0].base_stat}`)
-          return `${data.name}: Base ${data.stats[0].stat.name} is ${data.stats[0].base_stat}`
-        })
-       
-        arrPromise.push(promise)
-        // console.log(list[i])
-       }
-
-       console.log(arrPromise)
-       Promise.all(arrPromise).then(console.log)
-    })
-    // .then((list) => {
-    //   // let result = [];
-    //   for (let i = 0; i < list.length; i++) {
-    //     axios
-    //     .get(list[i])
-    //     .then(({data}) => {
-    //       if (data.stats[0].base_stat > 70 ) {
-    //         console.log(`${data.name}: Base ${data.stats[0].stat.name} is ${data.stats[0].base_stat}`)
-    //       }
-    //     })
-    //   }
-    // })
-   
-    .catch((error) => {
-      return error.message;
-    })
-      // .then((urlList) => {
-      //   urlList.forEach((url) => {
-      //     // console.log(data.stats)
-      //   })
-      // })
-)}
-
+        const arrPromise = [];
+        for (let i = 0; i < list.length; i++) {
+          const promise = axios.get(list[i]).then(({ data }) => {
+            // console.log(
+            //   `${data.name}: Base ${data.stats[0].stat.name} is ${data.stats[0].base_stat}`
+            // );
+            return `${data.name}: Base ${data.stats[0].stat.name} is ${data.stats[0].base_stat}`;
+          });
+          arrPromise.push(promise);
+        }
+        console.log(arrPromise);
+        return Promise.all(arrPromise).then(console.log);
+      })
+  );
+}
 // ====================================================================
-//    calling the if it exist function with inputs to the function
 // ====================================================================
+
+// .then((list) => {
+//   // let result = [];
+//   for (let i = 0; i < list.length; i++) {
+//     axios
+//     .get(list[i])
+//     .then(({data}) => {
+//       if (data.stats[0].base_stat > 70 ) {
+//         console.log(`${data.name}: Base ${data.stats[0].stat.name} is ${data.stats[0].base_stat}`)
+//     })
+// }
+// })
+
+//     .catch((error) => {
+//       return error.message;
+//     })
+//       // .then((urlList) => {
+//       //   urlList.forEach((url) => {
+//       //     // console.log(data.stats)
+//       //   })
+//       // })
+// )}
+// // ====================================================================
+// //    calling the if it exist function with inputs to the function
+// // ====================================================================
 const pokemonName = "";
+
 
 const result = showIfExists(pokemonName);
 console.log(result);
-// ====================================================================
-// function loop(result) {
-//   console.log(result)
-//   result.map((data) => console.log(data))
-//   console.log("hey")
-//   // result.map(({data}) => console.log(data))
-//   // result.map(({data}) => console.log(data))
+// ======================================================================================================
+// trying a different querry
+// ======================================================================================================
+
+// function getUSdata() {
+//   const url = `https://datausa.io/api/data?drilldowns=Nation&measures=Population`;
+//   result = [];
+//   axios
+//   .get(url)
+//   .then(({data}) => {
+//       // console.log(data.data)
+//       const allData = data.data;
+//       for (let i = 0; i < allData.length; i++) {
+//           const years = allData[i].Year
+//           result.push(allData[i]["ID Nation"])
+//           // console.log(years)
+//         }
+//         console.log(result)
+//   })
 // }
-// loop(result)
+// console.log(getUSdata())
+
+// ======================================================================================================
+// ======================================================================================================
+
+// function getAllNames() {
+//   const url = `https://swapi.dev/api/films/1/`;
+//   const charObj = {};
+//   return axios
+//     .get(url)
+//     .then(({ data }) => {
+//       const characters = data.characters;
+//       return characters;
+//     })
+//     .then((characters) => {
+//       for (let i = 0; i < characters.length; i++) {
+//         axios
+//         .get(characters[i])
+//         .then((result) => {
+//           const obj = {};
+//           obj[result.data.name] = { height: result.data.height, homeworld: result.data.homeworld};
+//           // console.log(obj);
+//           Object.assign(charObj, obj);
+//           // return charObj;
+//           // console.log(charObj);
+//         });
+//       }
+//       console.log(charObj);
+//     });
+// }
+// getAllNames();
 
 module.exports = {
   showIfExists,
+  // getUSdata,
+  // getAllNames,
 };
